08928034be07d80f119c66bf0d894f5357efba58
Scotty <reddirt@duck.com>
2022-04-04 20:30:52 -0500

    Updated docstrings, added log messages

--- helpers/configuration.py
+++ helpers/configuration.py
@@ -5,9 +5,18 @@ from helpers.logs import logging
 
 class Configuration:
     """
-     Class to create a configuration file, update get values from same.  By default, when the create_config_file
-     method is called it will create a configuration file with the name config.ini but can be change if desired.
-     If it already exists no action is taken
+     Class:
+
+     Responsible for creating and managing a configuration file.  By default, when the create_config_file
+     method is called it will create a configuration file with the name config.ini
+
+        Methods:
+     
+            create_config_file - Creates the configuration file.  Does not need to be called directly as it
+            will be called automatically with the add_config_section if the config.ini file is not found
+
+            add_config_section - Takes a list as an argument and adds the item names in the list as sections
+            to the configuration file if not already existing
     """
 
     def __init__(self, filename="config.ini"):
@@ -17,21 +26,21 @@ class Configuration:
 
     def create_config_file(self):
         """
-        create_config_file A method to create a configuration file in the root directory from with the scrip
-        is running
+        create_config_file A method to create a configuration file in the root directory
         """
 
         self.name = Path(self.path).joinpath(str(self.filename))
 
         if not self.name.is_file():
             self.name.touch()
+            logging.info(f"Created configuration file: {self.filename}")
         else:
-            return
+            logging.info(f"The configuration file, {self.filename} no need to create it")
 
     def add_config_sections(self, sections):
         """
-        add_config_sections A method to add sections to the config.ini file.  If the config file doesn't exis
-        it will attempt to create it by calling the create_config_file method
+        add_config_sections A method to add sections to the config.ini file.  If the config file doesn't exist
+        it will attempt to create it by calling the create_config_file method.  
 
         Args:
             sections (list): A list of sections to be added to the config.ini file.  If the section
@@ -46,7 +55,7 @@ class Configuration:
             try:
                 self.create_config_file()
             except Exception as e:
-                return e
+                logging.error(f"Creation of the configuration file errored with error message: {e}")
             else:
                 with open(config_file, "a") as file:
                     config.read(config_file)
@@ -56,7 +65,12 @@ class Configuration:
                             continue
                         else:
                             config.add_section(section)
+                            logging.info(f"Added {section} section to the configuration file")
 
                     config.write(file)
+                    logging.info(f"Configuration file has been updated with the provided sections")
+
+    def get_config_value(self, section, param):
+        pass
 
 
