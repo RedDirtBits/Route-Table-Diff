9c15ce0cd682a65a24c401bd81c6690231eaf6b4
Scotty <reddirt@duck.com>
2022-04-06 19:05:33 -0500

    Added handler to send messages to the console.

--- helpers/logs.py
+++ helpers/logs.py
@@ -6,8 +6,9 @@ import logging
 
 # Configure the logging
 logging.basicConfig(
-    filename="application.log",
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] (%(module)s):\n Message: %(message)s',
-    filemode='a',
-    )
+    handlers=[
+        logging.FileHandler("application.log", "a"),
+        logging.StreamHandler()
+    ])
